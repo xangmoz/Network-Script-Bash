e6f104e161a9002dc6848f6fb4e4febd3a05f009
antonio <xang813@gmail.com>
2024-02-16 15:23:05 -0500

    codigo final

--- NetworkScript.sh
+++ NetworkScript.sh
@@ -40,3 +40,31 @@ nslookup www.google.coom
 
 echo "......."
 
+#Check open ports on local machine
+
+read -p "enter the ip address or hostname to check open ports on: " remote_host
+echo "cheking open ports on $remote_host"
+nmap -p- $remote_host
+echo "..........."
+
+#traceroute to a remote host 
+read -p "enter teh ip address or hostname to trace route to: "trace_host
+echo " trace route to: $trace_host"
+tracert $trace_host
+echo "........................."
+
+
+
+#test Bandwith  using Speed test cli for windows
+read -p "do you want to test bandwith speedtest CLI? (y/n): "speedtest:option
+
+if ["speedtest_option == y"]; then
+./speedtestbyookla_64
+
+fi
+echo ".............."
+
+
+echo "network testing completed"
+
+
