00a4ddd697886dfae0b7033d82dce67e2ec712e5
antonio <xang813@gmail.com>
2024-02-16 14:00:41 -0500

    creacion de script de red

---
+++ NetworkScript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Network testing script for git bash on Windows
+
+
+echo "network testing script"
+
+#check Internet Connectivity
+
+
+echo "1.Cheking Internet Connectivity"
+
+
+ping -t 4 www.google.com
+
+
+
+echo "----------"
